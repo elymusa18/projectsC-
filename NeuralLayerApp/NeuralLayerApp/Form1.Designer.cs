dd58b7ee384f13e20c9415fc5ddf6105f1a0622b
Ellis Musa <98346161+elymusa18@users.noreply.github.com>
2026-05-10 17:07:57 +0300

    Add files via upload

---
+++ NeuralLayerApp/NeuralLayerApp/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace NeuralLayerApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalculateLayer = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCalculateLayer
+            // 
+            this.btnCalculateLayer.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnCalculateLayer.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculateLayer.Location = new System.Drawing.Point(179, 63);
+            this.btnCalculateLayer.Name = "btnCalculateLayer";
+            this.btnCalculateLayer.Size = new System.Drawing.Size(241, 86);
+            this.btnCalculateLayer.TabIndex = 0;
+            this.btnCalculateLayer.Text = "Calculate";
+            this.btnCalculateLayer.UseVisualStyleBackColor = false;
+            this.btnCalculateLayer.Click += new System.EventHandler(this.btnCalculateLayer_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(91, 198);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(91, 271);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(91, 330);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 25);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(631, 448);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCalculateLayer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalculateLayer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ NeuralLayerApp/NeuralLayerApp/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NeuralLayerApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btnCalculateLayer_Click(object sender, EventArgs e)
+        {
+            
+
+            double[] inputs = new double[] { 1, 2, 3, 2.5 };
+
+            // double[,] weights: [Неврон, Вход]
+            double[,] weights = new double[,] {
+                { 0.2, 0.8, -0.5, 1.0 },       // Тегла за Неврон 1 (индекс 0)
+                { 0.5, -0.91, 0.26, -0.5 },    // Тегла за Неврон 2 (индекс 1)
+                { -0.26, -0.27, 0.17, 0.87 }   // Тегла за Неврон 3 (индекс 2)
+            };
+
+            double[] bias = new double[] { 2, 3, 0.5 };
+            double[] outputs = new double[3]; // Масив за съхранение на 3-те изхода
+
+            // --- 2. Моделиране на слоя с вложени цикли ---
+
+            int numberOfNeurons = bias.Length; // 3
+            int numberOfInputs = inputs.Length; // 4
+
+           
+            for (int i = 0; i < numberOfNeurons; i++)
+            {
+                double neuronOutput = 0.0;
+
+                for (int j = 0; j < numberOfInputs; j++)
+                {
+               
+                    neuronOutput += inputs[j] * weights[i, j];
+                }
+
+                neuronOutput += bias[i];
+
+                outputs[i] = neuronOutput;
+            }
+
+          
+
+            // Резултати: 4.800, 1.210, 2.385
+
+            // Неврон 1 (i=0)
+            label1.Text = $"Неврон 1: {outputs[0]:F3}";
+
+            // Неврон 2 (i=1) 
+            label2.Text = $"Неврон 2: {outputs[1]:F3}";
+
+            // Неврон 3 (i=2)
+            label3.Text = $"Неврон 3: {outputs[2]:F3}";
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+
+    
+
